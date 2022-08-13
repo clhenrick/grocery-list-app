229a004aab927509fe656cd64e9890a4baae063d
Chris Henrick <chrishenrick@gmail.com>
2022-08-13 10:38:51 -0700

    render items as checkboxes

--- src/List.tsx
+++ src/List.tsx
@@ -5,10 +5,27 @@ interface Props {
 	data: IDatum[] | null;
 }
 
+function sanitize(string: string) {
+	return string.replace(/ /gi, '').toLowerCase();
+}
+
 export function List({ data }: Props) {
+
+	function renderItem({ item }:IDatum) {
+		const id = sanitize(item);
+		return (
+			<div key={id}>
+				<input id={id} type="checkbox" />{' '}
+				<label htmlFor={id}>
+					{item}
+				</label>
+			</div>
+		)
+	}
+
 	return (
-		<pre>
-			{ JSON.stringify(data, null, 2) }
-		</pre>
+		<div>
+			{ data?.filter(d => d.include).map(renderItem) }
+		</div>
 	);
 }
