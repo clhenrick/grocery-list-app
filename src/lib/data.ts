2b986a35bb75fec8ad6a5935c285e565f122485c
Chris Henrick <chrishenrick@gmail.com>
2022-08-13 10:06:32 -0700

    parse data as json

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,16 +1,41 @@
+import { csvParse, DSVRowString } from "d3-dsv"
+
 // https://docs.google.com/spreadsheets/d/1YMBkCqCi31xiVpWtSsw97YFMo0HwVE6N6hX3nv2fF-w/edit#gid=0
 const key = "1YMBkCqCi31xiVpWtSsw97YFMo0HwVE6N6hX3nv2fF-w";
 const sheet_name = "Sheet1";
-const sheetUrl = `https://docs.google.com/spreadsheets/d/${key}/gviz/tq?tqx=out:csv&sheet=${sheet_name}`
+const sheetUrl = `https://docs.google.com/spreadsheets/d/${key}/gviz/tq?tqx=out:csv&sheet=${sheet_name}`;
+
+export interface IDatum {
+	item: string;
+	category: string;
+	include: boolean;
+}
+
+interface IDatumString {
+	item: string;
+	category: string;
+	include: string;
+}
 
 export async function fetchSheetData() : Promise<[any|null, Error|null]> {
 	try {
 		const res = await window.fetch(sheetUrl);
-		console.log(res)
 		const text = await res.text();
-		console.log(text)
-		return [text, null];
+		const data = parseText(text);
+		return [data, null];
 	} catch (error: unknown) {
 		return [null, new Error("Failed to fetch")];
+	}
+}
+
+function parseText(text:string) : any {
+	return csvParse(text, row);
+}
+
+function row({ item, category, include }: DSVRowString<keyof IDatumString>) {
+	return {
+		item,
+		category,
+		include: include === "TRUE" ? true : false,
 	}
 }
