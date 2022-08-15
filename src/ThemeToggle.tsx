a9b8a4e804f89433755bcd5dc385b375ef4a60ad
Chris Henrick <chrishenrick@gmail.com>
2022-08-14 18:43:24 -0700

    use const enum in theme toggle & fix a11y toggle

--- src/ThemeToggle.tsx
+++ src/ThemeToggle.tsx
@@ -1,41 +1,50 @@
 import React, { useState, useEffect } from "react";
 import styles from "./ThemeToggle.module.css";
 
+const enum Theme {
+	LIGHT = "light",
+	DARK = "dark"
+}
+
+const enum ThemeClass {
+	LIGHT_THEME = "light-theme",
+	DARK_THEME = "dark-theme",
+}
+
 export function ThemeToggle() {
-	const DARK = "dark";
-	const LIGHT = "light";
-	const [theme, setTheme] = useState<"light"|"dark">(LIGHT);
+	const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
 
 	useEffect(() => {
 		const media = window.matchMedia("(prefers-color-scheme: dark)");
 		if (media.matches) {
-			setTheme("dark");
+			setTheme(Theme.DARK);
 		} else {
-			setTheme(LIGHT);
+			setTheme(Theme.LIGHT);
 		}
 	}, []);
 
 	useEffect(() => {
-		if (theme === LIGHT) {
-			document.body.classList.remove("dark-theme");
-			document.body.classList.add("light-theme");
+		if (theme === Theme.LIGHT) {
+			document.body.classList.remove(ThemeClass.DARK_THEME);
+			document.body.classList.add(ThemeClass.LIGHT_THEME);
 		} else {
-			document.body.classList.remove("light-theme");
-			document.body.classList.add("dark-theme");
+			document.body.classList.remove(ThemeClass.LIGHT_THEME);
+			document.body.classList.add(ThemeClass.DARK_THEME);
 		}
 	}, [theme]);
 
 	function handleClick() {
-		setTheme(theme === LIGHT ? DARK : LIGHT);
+		setTheme(theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT);
 	}
 
 	return (
 		<button
 			className={styles.ThemeToggle}
 			onClick={handleClick}
-			aria-label={`Switch to ${theme === LIGHT ? DARK : LIGHT} mode`}
+			aria-label={`Toggle dark theme`}
+			aria-pressed={theme === Theme.DARK}
 		>
-			{ theme === LIGHT ? "🌞" : "🌙"}
+			{ theme === Theme.LIGHT ? "🌞" : "🌙" }
 		</button>
 	);
 }
