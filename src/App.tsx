e6613cf37ecc3d6e35a0468b13bf796ff069ff04
Chris Henrick <chrishenrick@gmail.com>
2022-08-15 21:34:24 -0700

    handle reading from localstorage on first app render

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,40 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import { fetchSheetData, IDatum } from './lib/data';
+import { useStorage } from './lib/storage';
 import {List} from "./List";
 import {ThemeToggle} from "./ThemeToggle";
 
 function App() {
 	const [ data, setData ] = useState<IDatum[]|null>(null);
 	const [ error, setError ] = useState<Error|null>(null);
+	const [ writeSavedList, readSavedList] = useStorage();
 
+	// On first render/mount, try to load data from localStorage first.
+	// If it doesn't exist then try fetching it.
 	useEffect(() => {
-		async function handleData() {
+		async function handleFetch() {
 			const [data, error] = await fetchSheetData();
 			setData(data);
 			setError(error);
 		}
-		handleData();
+		try {
+			const savedList = readSavedList();
+			if (savedList && Array.isArray(savedList)) {
+				setData(savedList);
+			} else {
+				handleFetch();
+			}
+		} catch (error) {
+			setError(new Error("Problem reading localStorage."));
+		}
 	}, []);
+
+	// TODO: function that handles updating list items to be checked / unchecked.
+	// function updateListItem(id: number) {
+	// 	if (!data) return;
+	// 	setState()
+	// }
 
   return (
     <div className="App">
