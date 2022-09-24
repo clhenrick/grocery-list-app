c257e554ecb4267452f32dcbd21cfd51564f3821
Chris Henrick <chrishenrick@gmail.com>
2022-09-24 14:29:10 -0700

    prevent page scroll when menu modal is open

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 import { ResetButton } from "./ResetButton";
 import { ThemeToggle } from "./ThemeToggle";
 import { ToggleExcludedItems } from "./ToggleExcluded";
@@ -19,6 +21,19 @@ export function Menu({
   onToggleExcludedClick,
   showExcludedItems,
 }: MenuProps) {
+  useEffect(() => {
+    // https://css-tricks.com/prevent-page-scrolling-when-a-modal-is-open/
+    if (visible) {
+      document.body.style.position = "fixed";
+      document.body.style.top = `-${window.scrollY}px`;
+    } else {
+      const scrollY = document.body.style.top;
+      document.body.style.position = "";
+      document.body.style.top = "";
+      window.scrollTo(0, parseInt(scrollY || "0") * -1);
+    }
+  }, [visible]);
+
   return (
     <div className={`${styles.Menu} ${visible ? styles.visible : ""}`}>
       <button className={styles.MenuCloseBtn} onClick={onCloseClick}>
